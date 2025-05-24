9f416e7ff7e9af23e97c445570f68e3d87f77cfe
Kasse222 <kasselamine130@gmail.com>
2025-05-25 00:26:43 +0100

    ✅ FormRequest complet : Store/Update Tag + validation unique + API clean

--- app/Http/Requests/UpdateTagRequest.php
+++ app/Http/Requests/UpdateTagRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateTagRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateTagRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,8 +22,9 @@ class UpdateTagRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            //
+         return [
+            'name' => ['required', 'string', 'max:50', Rule::unique('tags', 'name')->ignore($this->tag->id)]
         ];
+
     }
 }
