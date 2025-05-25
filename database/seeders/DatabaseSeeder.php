b0d4ed538f4ebdfb323a258c8457452df6d4b125
Kasse222 <kasselamine130@gmail.com>
2025-05-25 19:35:25 +0100

    🧪 Seeders + Factories : données de test pour User, Post, Comment, Tag (relation complète)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,12 +11,25 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+        {
+            \App\Models\User::factory(10)
+            ->hasPosts(3)
+            ->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+            \App\Models\Tag::factory(5)->create();
+
+            // Associer des tags à chaque post
+            \App\Models\Post::all()->each(function ($post) {
+                $tags = \App\Models\Tag::inRandomOrder()->take(2)->pluck('id');
+                $post->tags()->attach($tags);
+            });
+
+            // Générer des commentaires
+            \App\Models\Post::all()->each(function ($post) {
+                \App\Models\Comment::factory(2)->create([
+                    'post_id' => $post->id,
+                    'user_id' => \App\Models\User::inRandomOrder()->first()->id,
+                ]);
+            });
+       }
 }
